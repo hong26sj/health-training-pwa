1130a3a599834905f30291545d16edd87e950f8c
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-09-06 16:06:38 +0900

    Refresh PWA cache for detailed visual update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='health-training-pwa-v2';
-const SHELL=['./','./index.html','./styles.css','./js/app.js','./js/data-source.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='health-training-pwa-v3';
+const SHELL=['./','./index.html','./styles.css','./charts.css','./js/app.js','./js/data-source.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
