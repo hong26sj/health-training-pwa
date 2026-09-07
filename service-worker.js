90bc3e3ef71acf030c77f2b6c5ed9a4b27929d1e
hong26sj <120166810+hong26sj@users.noreply.github.com>
2026-09-07 18:43:27 +0900

    Cache shared health model modules

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='health-training-pwa-v5';
-const SHELL=['./','./index.html','./styles.css','./charts.css','./nutrition.css','./js/app.js','./js/data-source.js','./js/nutrition-ui.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='health-training-pwa-v6';
+const SHELL=['./','./index.html','./styles.css','./charts.css','./nutrition.css','./js/app.js','./js/data-source.js','./js/health-model.js','./js/health-engine.js','./js/nutrition-ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
